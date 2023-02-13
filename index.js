8129857f0e3fdbcc1e0783b4fca0c6d2e5e592b1
sunil <sunilmoond.img@gmail.com>
2023-02-13 14:04:24 +0530

    changeRoutePath

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ let mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 let connection = require("./src/dbConnection/conn");
 
-let registerRoute = require("../Api/src/register/registerRoute/regRout");
-let loginRoute = require("../Api/src/login/loginRoute/loginRoute");
-let categoryRoute = require("../Api/src/category/categoryRoute/categoryRoute");
-let productRoute = require("../Api/src/product/productRoute/productRoute");
-const forgotPassRoute = require("../Api/src/forgotPassword/forgotPassRoute/forgotPassRoute");
-const mostPopularCoursesRoute = require("../Api/src/mostPopularCourses/mostPopularCoursesRoute/mostPopularCoursesRoute");
-const learningAtFingertipsRoute = require("../Api/src/learningAtFingertips/learningAtFingertipsRoute/learningAtFingertipsRoute");
-const addTocartRoute = require("../Api/src/addToCart/addToCartRoute/addToCartRoute");
-const courseDetailRoute=require('../Api/src/courseDetail/courseDetailRoute/courseDetailRoute')
+let registerRoute = require("./src/register/registerRoute/regRout");
+let loginRoute = require("./src/login/loginRoute/loginRoute");
+let categoryRoute = require("./src/category/categoryRoute/categoryRoute");
+let productRoute = require("./src/product/productRoute/productRoute");
+const forgotPassRoute = require("./src/forgotPassword/forgotPassRoute/forgotPassRoute");
+const mostPopularCoursesRoute = require("./src/mostPopularCourses/mostPopularCoursesRoute/mostPopularCoursesRoute");
+const learningAtFingertipsRoute = require("./src/learningAtFingertips/learningAtFingertipsRoute/learningAtFingertipsRoute");
+const addTocartRoute = require("./src/addToCart/addToCartRoute/addToCartRoute");
+const courseDetailRoute=require('./src/courseDetail/courseDetailRoute/courseDetailRoute')
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
